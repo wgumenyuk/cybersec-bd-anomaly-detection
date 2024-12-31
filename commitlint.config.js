f73b6894aa8806350785040c232f0f10944c2189
wgumenyuk <wlad@gumenyuk.de>
2024-12-31 01:34:32 +0100

    chore: initial commit

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+/**
+  @type {import("@commitlint/types").UserConfig}
+*/
+export default {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"chore",
+				"docs",
+				"feat",
+				"fix",
+				"merge",
+				"refactor",
+				"revert",
+				"test"
+			]
+		]
+	}
+};
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wgumenyuk/cybersec-bd-anomaly-detection
+
+go 1.23.3
